031c45ac64c159a19f8e6450654411af94e213fa
qiyuesam <1349893269@qq.com>
2026-04-19 10:36:30 +0800

    玩家的部分移动逻辑

--- Assets/Scipts/PlayerController.cs
+++ Assets/Scipts/PlayerController.cs
@@ -1,19 +1,58 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
+    public PlayerInputControls InputControls;
+    [SerializeField]
+    private Vector3 inputDirection;
+    private float moveSpeed=10f;
+    private float smoothTime=0.1f;
+    [SerializeField]
+    private Vector3 currentVelocity=Vector3.zero;
     // Start is called before the first frame update
     void Start()
     {
         rb=GetComponent<Rigidbody>();
+        InputControls=new PlayerInputControls();
+        InputControls.Gameplay.Enable();
     }
+    
 
     // Update is called once per frame
     void Update()
     {
-        
+        inputDirection=InputControls.Gameplay.Move.ReadValue<Vector3>();
     }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        var targetDirection=new Vector3(inputDirection.x,0,inputDirection.z).normalized;
+        if (targetDirection == Vector3.zero)
+        {
+            rb.velocity=new Vector3(0,rb.velocity.z,0);
+            return;
+        }
+        Vector3 targetVelocity=targetDirection*moveSpeed;
+        Vector3 smoothedVelocity = Vector3.SmoothDamp(
+            new Vector3(rb.velocity.x, 0, rb.velocity.z),
+            new Vector3(targetVelocity.x, 0, targetVelocity.z),
+            ref currentVelocity,
+            smoothTime,
+            Mathf.Infinity,
+            Time.fixedDeltaTime
+        );
+        rb.velocity = new Vector3(smoothedVelocity.x, rb.velocity.y, smoothedVelocity.z);
+    }
+    
+    
 }
--- Assets/Scipts/ServerTest/HelloWorldPlayer.cs
+++ Assets/Scipts/ServerTest/HelloWorldPlayer.cs
@@ -1,14 +1,17 @@
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace HelloWorld
 {
     public class HelloWorldPlayer : NetworkBehaviour
     {
         public NetworkVariable<Vector3> Position = new NetworkVariable<Vector3>();
+		private Rigidbody rb;
 
         public override void OnNetworkSpawn()
         {
+            rb=GetComponent<Rigidbody>();
             if (IsOwner)
             {
                 Move();
@@ -28,14 +31,25 @@ namespace HelloWorld
             Position.Value = randomPosition;
         }
 
+        
         static Vector3 GetRandomPositionOnPlane()
         {
+            
             return new Vector3(Random.Range(-3f, 3f), 1f, Random.Range(-3f, 3f));
         }
 
-        private void Update()
+        
+        private void FixedUpdate()
         {
-            transform.position = Position.Value;
+            if (!IsOwner)
+            {
+                if (rb != null)
+                {
+                    rb.MovePosition(Position.Value);
+                }
+                
+            }
+            
         }
     }
 }
